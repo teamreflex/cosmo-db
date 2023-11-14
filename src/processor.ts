b8f1dae16a0849270cf786bff793cacc4dafcb0e
xKairu <kairu@team-reflex.com>
2023-11-14 19:09:32 +1300

    set finality to 200

--- src/processor.ts
+++ src/processor.ts
@@ -33,7 +33,7 @@ export const processor = new EvmBatchProcessor()
       status: true,
     },
   })
-  .setFinalityConfirmation(100)
+  .setFinalityConfirmation(200)
   // artms objekts
   .addLog({
     address: [CONTRACT_ARTMS],
