5744158a14ac8b591fac0f9d074af82c63c646df
xKairu <kairu@team-reflex.com>
2024-01-13 18:12:00 +1300

    add read-only pg user

---
+++ db/migrations/1700720000000-Data.js
@@ -0,0 +1,39 @@
+module.exports = class Data1700720000000 {
+  name = "Data1700720000000";
+
+  async up(db) {
+    const dbName = process.env.DB_NAME;
+    const readUser = process.env.DB_READ_USER;
+    const readPass = process.env.DB_READ_PASS;
+
+    // create role
+    await db.query(
+      `CREATE ROLE "${readUser}" WITH LOGIN PASSWORD '${readPass}'`
+    );
+    // give connect privileges
+    await db.query(`GRANT CONNECT ON DATABASE "${dbName}" TO "${readUser}"`);
+    // allow public schema access
+    await db.query(`GRANT USAGE ON SCHEMA public TO "${readUser}"`);
+    // give select privileges to all tables
+    await db.query(
+      `GRANT SELECT ON ALL TABLES IN SCHEMA public TO "${readUser}"`
+    );
+    // give select privileges to all sequences
+    await db.query(
+      `GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO "${readUser}"`
+    );
+    // give select privileges to all future tables
+    await db.query(
+      `ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO "${readUser}"`
+    );
+    // give select privileges to all future sequences
+    await db.query(
+      `ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO "${readUser}"`
+    );
+  }
+
+  async down(db) {
+    const readUser = process.env.DB_READ_USER;
+    await db.query(`DROP USER IF EXISTS "${readUser}"`);
+  }
+};
