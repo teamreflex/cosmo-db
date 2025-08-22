5783547eb38e03ce21ba75fcd56cd3298e537218
kylekz <kairu@team-reflex.com>
2025-08-23 00:58:34 +1200

    fix case

--- db/migrations/1755862697000-Data.js
+++ db/migrations/1755862697000-Data.js
@@ -2,16 +2,16 @@ module.exports = class Data1755862697000 {
   name = "Data1755862697000";
 
   async up(db) {
-    // add bandImageUrl column to collection table
+    // add band_image_url column to collection table
     await db.query(
-      `ALTER TABLE "public"."collection" ADD COLUMN "bandImageUrl" varchar(255);`
+      `ALTER TABLE "public"."collection" ADD COLUMN "band_image_url" varchar(255);`
     );
   }
 
   async down(db) {
-    // drop bandImageUrl column from collection table
+    // drop band_image_url column from collection table
     await db.query(
-      `ALTER TABLE "public"."collection" DROP COLUMN "bandImageUrl";`
+      `ALTER TABLE "public"."collection" DROP COLUMN "band_image_url";`
     );
   }
 };
